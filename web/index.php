a1b7ae7e19f74dfa73bf402ffe2f7b3008f37eaa
hjgdfkudnxhfd <valeev@rtural.ru>
2025-05-04 17:53:23 +0500

    remove merge

--- web/index.php
+++ web/index.php
@@ -1,48 +1,14 @@
 <?php
-use Slim\Factory\AppFactory;
-use NastyaKuznet\Blog\Controller\PostController;
-use NastyaKuznet\Blog\Middleware\RoleMiddleware;
-use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
-use Slim\Routing\RouteCollectorProxy;
+require_once '../src/Base/db.php';
+require_once '../src/Base/DatabaseService.php';
 
-// 1. Создаем контейнер
-$containerBuilder = new \DI\ContainerBuilder();
-$containerBuilder->addDefinitions(__DIR__ . '/../src/app/config/dependencies.php');
-$container = $containerBuilder->build();
-// 2. Создаем Slim приложение, передавая контейнер
-$app = AppFactory::createFromContainer($container);
+use Base\DatabaseService;
 
-// Add Twig-View Middleware
-$app->add(TwigMiddleware::createFromContainer($app));
+// Создаем экземпляр сервиса
+$databaseService = new DatabaseService($pdo);
 
-// Add Routing Middleware
-$app->addRoutingMiddleware();
-
-// Add Error Middleware
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-// Dependency Injection Container (DI Container)
-$container = $app->getContainer();
-
-// Группировка роутов по префиксу 'post'
-$app->group('/post', function (RouteCollectorProxy $group) {
-    // Роуты, требующие роль 'writer' или выше
-    $group->get('/create', [PostController::class, 'create'])->add(new RoleMiddleware(['writer', 'moder', 'admin']));
-    $group->post('/create', [PostController::class, 'create'])->add(new RoleMiddleware(['writer', 'moder', 'admin']));
-    // Роуты, требующие роль 'moder' или выше
-    $group->get('/edit/{id}', [PostController::class, 'edit'])->add(new RoleMiddleware(['moder', 'admin']));
-    $group->post('/edit/{id}', [PostController::class, 'edit'])->add(new RoleMiddleware(['moder', 'admin']));
-});
-
-$app->get('/', [PostController::class, 'index']);
-
-$app->map(['GET', 'POST'],'/post/{id}', [PostController::class, 'show']);
-
-$app->post('/post/{id}/like', [PostController::class, 'likePost']);
-
-//Заглушки для admins
-$app->get('/users', [PostController::class, 'users'])->add(new RoleMiddleware(['admin']));
-
-
-$app->run(); 
+// Пример вывода всех постов
+$posts = $databaseService->getAllPosts();
+echo "<pre>";
+print_r($posts);
+echo "</pre>";
